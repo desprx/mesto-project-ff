77cf5c48771e92771b64872f7b8c22ddcc115ff9
desprx <desprz@yandex.ru>
2024-02-24 21:42:02 +0300

    Добавление и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+const cardItem = cardTemplate.querySelector(".places__item");
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(item) {
+  const cardFilling = cardTemplate.cloneNode(true);
+  const deleteBtn = cardFilling.querySelector(".card__delete-button");
+
+  cardFilling.querySelector(".card__image").src = item.link;
+  cardFilling.querySelector(".card__image").alt = item.name;
+  cardFilling.querySelector(".card__title").textContent = item.name;
+
+  deleteBtn.addEventListener("click", deleteCard);
+  cardList.append(cardFilling);
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(event) {
+  const cardItem = event.target.closest(".places__item");
+  cardItem.remove();
+}
+
 // @todo: Вывести карточки на страницу
+initialCards.forEach(addCard);
